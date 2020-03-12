d8b33feda53e2d1ff0b5e5cab0cf2a802a1e86fc
PartumGames <37095216+PartumGames@users.noreply.github.com>
2020-03-11 22:55:54 -0500

    Initial Commit

---
+++ Scripts/RTS_Cam.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTS_Cam : MonoBehaviour
+{
+    public Camera cam;
+    public float zoomSpeed;
+    public Vector2 mouseLook;
+    public float sensitivity;
+    public float moveSpeed;
+
+    private Vector3 startPos;
+
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            transform.position = startPos;
+        }
+
+
+        HandleMove();
+
+        HandleRot();
+
+        HandleZoom();
+
+    }
+
+    private void HandleMove()
+    {
+        //--moves forward/back/left/right relative to tranforms rotation
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(x, 0f, z) * moveSpeed * Time.deltaTime;
+
+        transform.position += transform.TransformDirection(moveDirection);
+
+    }
+
+    private void HandleRot()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            float horizontal = Input.GetAxis("Mouse X");
+            float vertical = Input.GetAxis("Mouse Y");
+
+            Vector2 look = new Vector2(horizontal, vertical);
+            mouseLook += look * sensitivity;
+
+            //mouseLook.y = Mathf.Clamp(mouseLook.y, 30, -70);
+
+            //cam.transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+            transform.localRotation = Quaternion.AngleAxis(mouseLook.x, transform.up);
+        }
+    }
+
+    private void HandleZoom()
+    {
+        float zoom = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        cam.fieldOfView += -zoom;
+
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 40f, 80f);
+    }
+}
---
+++ Scripts/Unit.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+
+
+    private NavMeshAgent myAgent;
+
+    private GameObject selectorIcon;
+
+    private Vector3 startingPos;
+
+    private Vector3 offset;
+
+
+    private void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        selectorIcon = transform.GetChild(0).gameObject;
+        selectorIcon.SetActive(false);
+        startingPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            myAgent.SetDestination(startingPos);
+        }
+    }
+
+    public void SelectUnit()
+    {
+        selectorIcon.SetActive(true);
+    }
+
+    public void DeselectUnit()
+    {
+        selectorIcon.SetActive(false);
+        offset = Vector3.zero;
+    }
+
+    public void CalculateOffset(Vector3 _center)
+    {
+        Vector3 center = new Vector3(_center.x, transform.position.y, _center.z);
+        offset = center - transform.position;
+    }
+
+    public void MoveToSpot(Vector3 _pos)
+    {
+        Vector3 pos = new Vector3(_pos.x, transform.position.y, _pos.z);
+        Vector3 moveToPos = pos + offset;
+        myAgent.SetDestination(moveToPos);
+    }
+
+    public void MoveToSpot(Vector3 _pos, Vector3 _center)
+    {
+        Vector3 center = new Vector3(_center.x, transform.position.y, _center.z);
+        Vector3 pos = new Vector3(_pos.x, transform.position.y, _pos.z);
+
+        offset = center - transform.position;
+        Vector3 moveToPos = pos + offset;
+        myAgent.SetDestination(moveToPos);
+    }
+
+}
